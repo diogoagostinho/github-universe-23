16b7595ca87d7f76f43ddad154c3a4a204443689
AGOSTINHO <LaaGostim.OFICIAL@gmail.com>
2023-12-27 17:55:38 +0000

    r0.3.0
    + Added icons for description
    + More styles
    + More structure
    + Scripts
    
    Need to find a way to make the gallery work (either flexbox or grid)

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Hero Video
 const heroPlayButton = document.getElementById("heroPlayButton");
 const heroImage = document.getElementById("heroImage");
 const heroContent = document.getElementById("heroContent");
@@ -23,3 +24,32 @@ navbarLogo.addEventListener("mouseover", function () {
 navbarLogo.addEventListener("mouseout", function () {
   navbarArrow.style.opacity = "0";
 });
+
+//
+//Hero Image
+//
+
+const heroLogo = document.getElementById("heroLogo");
+
+window.addEventListener("scroll", function () {
+  heroLogo.style.opacity = 1;
+  heroLogo.style.transform = "scale(1)";
+});
+
+awakePage();
+
+function awakePage() {
+  heroLogo.style.opacity = checkVisible(heroLogo) ? 1 : 0;
+  heroLogo.style.transform = checkVisible(heroLogo)
+    ? "scale(1)"
+    : "scale(1.05)";
+}
+
+function checkVisible(elm) {
+  var rect = elm.getBoundingClientRect();
+  var viewHeight = Math.max(
+    document.documentElement.clientHeight,
+    window.innerHeight
+  );
+  return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+}
